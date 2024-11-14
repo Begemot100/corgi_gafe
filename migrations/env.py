82cb481cd71492b921c72c5d4621dac7edd8674c
Begemot100 <you@example.com>
2024-11-14 21:05:30 +0100

    Упрощение env.py с удалением использования current_app

--- migrations/env.py
+++ migrations/env.py
@@ -33,7 +33,11 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True  # Это добавлено для сравнения типов столбцов
+        )
 
         with context.begin_transaction():
             context.run_migrations()
