9f27d7ef69cfae7a7a64958988519734745679d7
Begemot100 <you@example.com>
2024-11-14 20:58:20 +0100

    Настроен контекст приложения для Alembic в env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,15 @@
-import logging
 from logging.config import fileConfig
-
+import logging
+from alembic import context
+from app import app
 from flask import current_app
 
-from alembic import context
+config = context.config
+
+# Устанавливаем контекст приложения
+with app.app_context():
+    config.set_main_option('sqlalchemy.url', app.config['SQLALCHEMY_DATABASE_URI'])
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
