2cbdd8ccb1f152a0186e22844fd90e6ec4ae8898
Begemot100 <you@example.com>
2024-11-14 21:18:15 +0100

    Обновление env.py для явной установки URL базы данных

--- migrations/env.py
+++ migrations/env.py
@@ -1,28 +1,27 @@
 import logging
 from alembic import context
-from flask import current_app
-from app import app, db  # импортируйте ваше приложение и db из основного файла
+from flask import Flask
+from app import db, app  # импорт приложения и db
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('alembic.runtime.migration')
 
-# Устанавливаем URL базы данных для Alembic
 config = context.config
 config.set_main_option('sqlalchemy.url', app.config['SQLALCHEMY_DATABASE_URI'])
 target_metadata = db.metadata
 
-# Устанавливаем контекст приложения для Alembic
+# Устанавливаем контекст приложения
 with app.app_context():
     def run_migrations_online():
-        """Запуск миграций в режиме онлайн с контекстом приложения."""
+        """Запуск миграций в режиме онлайн."""
         connectable = db.engine
 
         with connectable.connect() as connection:
             context.configure(
                 connection=connection,
                 target_metadata=target_metadata,
-                compare_type=True  # сравнение типов для обнаружения изменений
+                compare_type=True
             )
             with context.begin_transaction():
                 context.run_migrations()
