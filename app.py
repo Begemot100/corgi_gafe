52354eabb26c1cf80c7868b508085f307ef30f71
Begemot100 <you@example.com>
2024-11-17 09:04:08 +0100

    Fix database URL

--- app.py
+++ app.py
@@ -19,14 +19,14 @@ from apscheduler.schedulers.background import BackgroundScheduler
 # Настройка логгирования
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация приложения Flask
 app = Flask(__name__)
 
 # Настройка базы данных: используем DATABASE_URL, если доступно, или SQLite для локальной разработки
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-    'DATABASE_URL',
-    f"sqlite:///{os.path.join(os.getcwd(), 'instance', 'employees.db')}"
-)
+db_url = os.getenv('DATABASE_URL')
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url or f"sqlite:///{os.path.join(os.getcwd(), 'instance', 'employees.db')}"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '6006'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=4)
